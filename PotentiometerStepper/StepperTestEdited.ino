2904784cfdb909efdd4fb73fc44c0baa1bca03af
Mika Yeap <mikaomarr@gmail.com>
2017-12-11 12:16:21 +0800

    Initial commit

---
+++ PotentiometerStepper/StepperTestEdited.ino
@@ -0,0 +1,48 @@
+/* 
+This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
+It won't work with v1.x motor shields! Only for the v2's with built in PWM
+control
+
+For use with the Adafruit Motor Shield v2 
+---->	http://www.adafruit.com/products/1438
+*/
+
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+// Or, create it with a different I2C address (say for stacking)
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+
+// Connect a stepper motor with 200 steps per revolution (1.8 degree)
+// to motor port #2 (M3 and M4)
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
+int potpin = 0;  // analog pin used to connect the potentiometer
+int initialVal = 0; // previous val
+int val = 0;    // variable to read the value from the analog pin
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Stepper test!");
+
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  
+  myMotor->setSpeed(300);  // 10 rpm   
+}
+
+void loop() {
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  val = map(val, 0, 1023, 0, 200);
+
+  if (val > initialVal) {
+    initialVal += 1; 
+    myMotor->step(1, FORWARD, MICROSTEP);
+  } else if (val < initialVal) {
+    initialVal -= 1; 
+    myMotor->step(1, BACKWARD, MICROSTEP);
+  } 
+}
